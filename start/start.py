b0b139a530003e44be54f6f4345b9cdad1149db2
Nikita <pechka959@gmail.com>
2025-01-09 21:27:44 +0300

    Update start.py

--- start/start.py
+++ start/start.py
@@ -27,35 +27,35 @@ authorizedBrowser = AuthorizationSmoke("chrome", "https://staging.sphera.work/")
 def test_auth():
     authorizedBrowser.authorization("9888888888", "8888")
    
-# def test_01():
-#     name_channel = generate_random_string(10)
-#     info_channel = generate_random_string(20)
-#     authorizedBrowser.test_01_create_channel_add_member_delete_channel(name_channel, info_channel)
-#     authorizedBrowser.test_02_create_and_archive_channel(name_channel, info_channel)    
+def test_01():
+    name_channel = generate_random_string(10)
+    info_channel = generate_random_string(20)
+    authorizedBrowser.test_01_create_channel_add_member_delete_channel(name_channel, info_channel)
+    authorizedBrowser.test_02_create_and_archive_channel(name_channel, info_channel)    
     
 
-# def test_03():
-#     authorizedBrowser.test_04_mention_member()
+def test_03():
+    authorizedBrowser.test_04_mention_member()
 
-# def test_04():
-#     authorizedBrowser.test_05_pinned_mesagges()
+def test_04():
+    authorizedBrowser.test_05_pinned_mesagges()
 
 
-# def test_06():
-#     authorizedBrowser.test_07_open_discussions_go_to_message()
+def test_06():
+    authorizedBrowser.test_07_open_discussions_go_to_message()
 
 def test_07():
     authorizedBrowser.my_profile_settings()
 
 
 # @pytest.mark.skip
-# def test_02():
-#     name_channel = generate_random_string(10)
-#     authorizedBrowser.test_03_pinned_message(name_channel)
+def test_02():
+    name_channel = generate_random_string(10)
+    authorizedBrowser.test_03_pinned_message(name_channel)
 
     # @pytest.mark.skip
-# def test_05():
-#     name_channel = generate_random_string(10)
-#     authorizedBrowser.test_06_open_discussions(name_channel)
+def test_05():
+    name_channel = generate_random_string(10)
+    authorizedBrowser.test_06_open_discussions(name_channel)
 #####################################################################################################################################
 
